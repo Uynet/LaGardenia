4f3530c61d7f4ef6f3d9c660669fdf6c967e4a04
uynet <highsate@gmail.com>
2021-04-16 23:33:06 +0900

    wip-loading

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,27 @@ import "../styles/global.scss";
 import adobeloader from "../adobefont";
 import { useEffect } from "react";
 
+let isLoadComplete = false;
+let loadCompleteImageNum = 0;
+const po = () => {
+  console.log("complete");
+  loadCompleteImageNum++;
+  if (loadCompleteImageNum == 1) {
+    isLoadComplete = true;
+    console.log("complete");
+  }
+};
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    if (process.browser) adobeloader(document);
+    if (process.browser) {
+      adobeloader(document);
+
+      const image = new Image();
+      image.src = "/images/bg.png";
+      image.onload = po;
+      console.log(image);
+      console.log("unko");
+    }
   }, []);
   return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
+import React from "react";
 import Layout from "../components/layout";
-
-export default function Home() {
-  return (
-    <Layout home>
-      <Head>
-        <title>{"LA GARDENIA"}</title>
-      </Head>
-    </Layout>
-  );
+export default class Home extends React.Component {
+  render() {
+    return (
+      <Layout home>
+        <Head>
+          <title>{"LA GARDENIA"}</title>
+        </Head>
+      </Layout>
+    );
+  }
 }
