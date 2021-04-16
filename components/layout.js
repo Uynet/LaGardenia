ab57b16f0f0bb60cab919e42d7b1b4e14c529df5
uynet <highsate@gmail.com>
2021-04-16 11:29:31 +0900

    animate-flame

--- components/layout.js
+++ components/layout.js
@@ -1,40 +1,21 @@
-import Image from 'next/image'
-import styles from './layout.module.scss'
-import utilStyles from '../styles/utils.module.scss'
-import Link from 'next/link'
-import Head from "next/head"
+import Image from "next/image";
+import styles from "./layout.module.scss";
+import utilStyles from "../styles/utils.module.scss";
+import Link from "next/link";
+import Head from "next/head";
 
-const name = 'uynet'
-export const siteTitle = 'こんにちわ'
+const name = "uynet";
+export const siteTitle = "LaGardenia";
 
-export default function Layout({ children,home }) {
-  return <div className={styles.container}>
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
       <Head>
-          <title>{siteTitle}</title>
+        <title>{siteTitle}</title>
       </Head>
-      <header className={styles.header}>
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/uychan.png"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-          </>
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>←</a>
-          </Link>
-        </div>
-      )}
+      <div>
+        <img className={styles.imgFlame} src="images/flame.png" />
+      </div>
     </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,13 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.scss'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import Post from './posts/[id]'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.scss";
 
-//builtin func
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-function PostData({id , date , title}){
-  return(
-            <div className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a> {title} </a>
-              </Link>
-              <br />
-                <small className={utilStyles.lightText}>
-                  <Date dateString={date} />
-                </small>
-            </div>
-  )
-}
-
-export default function Home({allPostsData}) {
+export default function Home() {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <h2 className={utilStyles.headingLg}>ゆいブログ</h2>
-        <ul className={utilStyles.list}>
-        {allPostsData.map(({ id, date, title }) => (
-            <PostData key={id} id={id} date={date} title={title}/>
-          ))}
-        </ul>
-      </section>
     </Layout>
-  )
+  );
 }
