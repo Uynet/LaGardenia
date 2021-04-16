a133c2d052071765e85e52fc7d348f8d4a080d11
uynet <highsate@gmail.com>
2021-04-16 20:32:48 +0900

    flame

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,15 @@ import Head from "next/head";
 
 export const siteTitle = "LaGardenia";
 
+function Copyright() {
+  return (
+    <div>
+      <div className={styles.copyright}>
+        Copyright© 2021 uynet.work All Rights Reserved.
+      </div>
+    </div>
+  );
+}
 function Info() {
   return (
     <div>
@@ -17,6 +26,29 @@ function TrackList() {
       <div>曲1</div>
       <div>曲2</div>
       <div>曲3</div>
+    </div>
+  );
+}
+
+function Flames() {
+  return (
+    <div>
+      <img
+        src="/images/flame-LT.png"
+        className={styles.flameLT + " " + styles.flame}
+      />
+      <img
+        src="/images/flame-LT.png"
+        className={styles.flameRT + " " + styles.flame}
+      />
+      <img
+        src="/images/flame-LT.png"
+        className={styles.flameLB + " " + styles.flame}
+      />
+      <img
+        src="/images/flame-LT.png"
+        className={styles.flameRB + " " + styles.flame}
+      />
     </div>
   );
 }
@@ -54,6 +86,7 @@ export default function Layout() {
       <div className={styles.intro}>
         <img className={styles.imgFlame} src="images/flame.png" />
         <img className={styles.loadingLogo} src="images/logo.png" />
+        <Flames />
       </div>
       {/*<div className={styles.screen1} />*/}
       <div className={styles.jacketBGDummy} />
@@ -61,10 +94,17 @@ export default function Layout() {
         <div className={styles.bgDiv} />
         {/*<img className={styles.angel} src="images/angel.png" />*/}
       </div>
+      <div className={styles.block} />
       <div className={styles.container}>
         <Info />
         <TrackList />
         <Credit />
+        <img
+          src="images/angel-mini.png"
+          className={styles.mini}
+          width="200px"
+        />
+        <Copyright />
       </div>
     </div>
   );
