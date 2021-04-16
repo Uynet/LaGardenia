aa08bb8ca441a5c35336f89ec14b03db95cf1ff4
uynet <highsate@gmail.com>
2021-04-16 12:17:54 +0900

    ad-bg

--- components/layout.js
+++ components/layout.js
@@ -9,6 +9,10 @@ export default function Layout() {
         <img className={styles.imgFlame} src="images/flame.png" />
         <img className={styles.loadingLogo} src="images/logo.png" />
         {/*<div className={styles.screen1} />*/}
+        <div className={styles.jacketBG}>
+          <img className={styles.bg} src="images/bg.png" />
+          <img className={styles.angel} src="images/angel.png" />
+        </div>
       </div>
     </div>
   );
