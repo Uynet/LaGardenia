da6598113e0fc3b4b94c11100ad9a410e7faea06
uynet <highsate@gmail.com>
2021-04-16 12:00:13 +0900

    screen-animation

--- components/layout.js
+++ components/layout.js
@@ -1,21 +1,14 @@
-import Image from "next/image";
 import styles from "./layout.module.scss";
-import utilStyles from "../styles/utils.module.scss";
-import Link from "next/link";
-import Head from "next/head";
 
-const name = "uynet";
 export const siteTitle = "LaGardenia";
 
-export default function Layout({ children, home }) {
+export default function Layout() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
       <div>
         <img className={styles.imgFlame} src="images/flame.png" />
         <img className={styles.loadingLogo} src="images/logo.png" />
+        {/*<div className={styles.screen1} />*/}
       </div>
     </div>
   );
