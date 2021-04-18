f86d622a90f6c88e05655cbcaa6542bb315b8b2b
uynet <highsate@gmail.com>
2021-04-18 21:40:43 +0900

    ogp-type

--- components/layout.js
+++ components/layout.js
@@ -191,6 +191,8 @@ export default function Layout() {
         <meta property="og:title" content="LA GARDENIA" />
         <meta property="og:site_name" content="LA GARDENIA" />
         <meta property="og:description" content="永遠の箱庭" />
+        <meta property="og:url" content="lagardenia.uynet.work" />
+        <meta property="og:type" content="website" />
         <meta
           property="og:image"
           content="https://lagardenia.uynet.work/images/jacket_omote.png"
